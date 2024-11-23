174bb13dc36d802d4bf26dacd99ad2bb2c9431bc
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-11-23 11:59:37 +0530

    Add files via upload

---
+++ NumberSwap/main.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the 1st number"))
+num2 = int(input("Enter the 2nd number"))
+num3 = int(input("Enter the 3rd number"))
+
+x = num1 
+num1 = num2 
+num2 = num3 
+num3 = x
+
+print("the swapped values are:" ,num1, num2, num3 )
